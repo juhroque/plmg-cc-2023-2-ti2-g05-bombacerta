8a7e009120ec8a99107143152c02d45cc3c618a9
juhroque <juliapinheiroroque960@gmail.com>
2023-11-22 19:07:21 -0300

    remove port

--- src/main/java/com/ti2cc/App.java
+++ src/main/java/com/ti2cc/App.java
@@ -111,7 +111,7 @@ public class App {
      * @param args
      */
     public static void main(String[] args) {
-        port(1111);
+       // port(1111);
 
         staticFiles.location("/public");
         
